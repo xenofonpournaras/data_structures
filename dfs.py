3d47e853b7cae0e8fa07959fe0adc07e042d9b86
xenofonpournaras <64291154+xenofonpournaras@users.noreply.github.com>
2024-09-05 14:57:59 +0200

    Add files via upload

---
+++ dfs.py
@@ -0,0 +1,19 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+visited = set()
+
+def dfs(visited, graph, root):
+    if root not in visited:
+        print(root)
+        visited.add(root)
+        for neighbour in graph[root]:
+            dfs(visited, graph, neighbour)
+
+dfs(visited, graph, 'A')
